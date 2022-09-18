5ca37e714163e606aee206f9d611d31ca33922cf
Flynn <ppstream123@126.com>
2022-09-18 22:54:13 +0800

    🎉 init

---
+++ src/components/Tweet.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { isDark } from '../logics'
+
+onMounted(() => {
+  // @ts-expect-error cdn
+  window?.twttr.widgets.load()
+})
+</script>
+
+<template>
+  <div class="">
+    <blockquote class="twitter-tweet" :data-theme="isDark ? 'dark' : 'light'">
+      <slot />
+    </blockquote>
+  </div>
+</template>
